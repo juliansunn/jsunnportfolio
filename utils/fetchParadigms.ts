dd8ffba1fc2189c200b2f35d29eec0e42dd06933
juliansunn <juliansunn@gmail.com>
2023-07-01 08:08:51 -0700

    console log debugging

--- utils/fetchParadigms.ts
+++ utils/fetchParadigms.ts
@@ -4,6 +4,11 @@ export const fetchParadigms = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getParadigms`
   );
+  console.log(
+    "process.env.NEXT_PUBLIC_BASE_URL",
+    process.env.NEXT_PUBLIC_BASE_URL
+  );
+  console.log("res", res);
   const data = await res.json();
   const paradigms: Paradigm[] = data.paradigms;
 
